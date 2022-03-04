408d65df2646c26c47179ad41d7fddebef807dda
100SM <bsm2716@gmaile.com>
2022-03-04 09:50:14 +0900

    express #1

--- helloweb-ex02/routes/hello.js
+++ helloweb-ex02/routes/hello.js
@@ -3,9 +3,11 @@ const express = require("express");
 const router = express.Router();
 
 router.route("/01").get(function (req, res) {
-  res.writeHead(200, {
-    "Content-Type": "text/html",
-  });
-  res.end("<h1>/hello/01</h1>");
+  const data = {
+    no: req.query.no,
+    email: req.query.email,
+  };
+
+  res.render("hello/01", data);
 });
 module.exports = router;
---
+++ helloweb-ex02/routes/user.js
@@ -0,0 +1,8 @@
+const express = require("express");
+
+const router = express.Router();
+
+// router.route("/user").get(function (req, res) {
+//   res.render("user/index", data);
+// });
+module.exports = router;
